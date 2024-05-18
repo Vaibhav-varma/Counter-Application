1f1751ef24811986caa82478d00b24df23289082
vaibhav varma <vaibhavvarma5327@gmail.com>
2024-05-18 16:50:30 +0530

    second changes

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const countInfo = document.getElementById("count");
-const btnDecrease = document.getElementById("decrease");
-const btnReset = document.getElementById("reset");
-const btnIncrease = document.getElementById("increase");
+const value = document.getElementById("value");
+const buttons = document.querySelectorAll(".btn");
 
 let count = 0;
 
 function updateCountColor() {
-    let countInfoClass = countInfo.classList;
+    let valueClass = value.classList;
+    
     if(count > 0) {
-        countInfo.classList.remove(countInfoClass);
-        countInfo.classList.add("green");
+        value.classList.remove(valueClass);
+        value.classList.add("green");
     } else if(count < 0) {
-        countInfo.classList.remove(countInfoClass);
-        countInfo.classList.add("red");
+        value.classList.remove(valueClass);
+        value.classList.add("red");
     } else {
-        countInfo.classList.remove(countInfoClass);
-        countInfo.classList.add("black");
+        value.classList.remove(valueClass);
+        value.classList.add("black");
     }
 }
 
-btnIncrease.addEventListener("click", () => {
-    count++;
-    console.log(count);
-    countInfo.textContent = count;
-    updateCountColor();
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        const button = event.target.id;
+        if(button === "increase") {
+            count++;
+            value.textContent = count;
+        } else if(button === "decrease") {
+            count--;
+            value.textContent = count;
+        } else {
+            count = 0;
+            value.textContent = count;
+            
+        }
+        updateCountColor();
+    });
 });
 
-btnDecrease.addEventListener("click", () => {
-    count--;
-    console.log(count);
-    countInfo.textContent = count;
-    updateCountColor();
-});
 
-btnReset.addEventListener("click", () => {
-    count = 0;
-    console.log(count);
-    countInfo.textContent = count;
-    updateCountColor();
-});
+
+
+
 
 
 
